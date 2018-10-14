5a066abc786aea37a813c0edfa7d1c7e4f4b567a
Jana Bergant <jana.bergant@gmail.com>
2018-10-14 08:50:37 +0200

    s10 -004 – Response format

--- client/src/components/chatbot/QuickReply.js
+++ client/src/components/chatbot/QuickReply.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const QuickReply = (props) => {
-    if (props.reply.structValue.fields.payload) {
+    if (props.reply.payload) {
         return (
+<<<<<<< HEAD
             <a style={{ margin: 3}} className="btn-floating btn-large waves-effect waves-light blue"
                onClick={() =>
                    props.click(
@@ -11,13 +12,24 @@ const QuickReply = (props) => {
                    )}
                >
                 {props.reply.structValue.fields.text.stringValue}
+=======
+            <a style={{ margin: 3}} onClick={() => props.click(props.reply.payload, props.reply.text)}
+               className="btn-floating btn-large waves-effect waves-light blue">
+                {props.reply.text}
+>>>>>>> 2057bad... s10 -004 – Response format
             </a>
         );
     } else {
         return (
+<<<<<<< HEAD
             <a style={{ margin: 3}} className="btn-floating btn-large waves-effect waves-light blue"
                href={props.reply.structValue.fields.link.stringValue}>
                 {props.reply.structValue.fields.text.stringValue}
+=======
+            <a style={{ margin: 3}} href={props.reply.link}
+               className="btn-floating btn-large waves-effect waves-light blue">
+                {props.reply.text}
+>>>>>>> 2057bad... s10 -004 – Response format
             </a>
         );
     }
